6525e6af87e032a8eb747e288f629f5c403a249b
Mohan Krishna Koppula <144883962+mohankoppula45@users.noreply.github.com>
2024-09-05 14:15:36 +0530

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Number{
+    public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0;
+        
+        
+    }
+}
